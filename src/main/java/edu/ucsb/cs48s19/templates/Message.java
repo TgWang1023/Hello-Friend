b4f0c65e9b1aee34288081fb537d3aa07be4d28c
Ken Xu <33238360+ChangweiXu@users.noreply.github.com>
2019-04-21 15:28:33 -0700

    kx: add templates directory

---
+++ src/main/java/edu/ucsb/cs48s19/templates/Message.java
@@ -0,0 +1,18 @@
+package edu.ucsb.cs48s19.templates;
+
+public class Message {
+
+    private String content;
+
+    public Message() {
+    }
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
