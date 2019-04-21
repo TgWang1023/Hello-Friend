cf4b37c2bbd03bc7e1c737f8ea2e6019f98c2e31
Ken Xu <33238360+ChangweiXu@users.noreply.github.com>
2019-04-21 15:36:07 -0700

    kx: try: get session ID by interceptor

--- src/main/java/edu/ucsb/cs48s19/hellofriend/WebSocketConfig.java
+++ src/main/java/edu/ucsb/cs48s19/hellofriend/WebSocketConfig.java
@@ -1,10 +1,18 @@
 package edu.ucsb.cs48s19.hellofriend;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -12,7 +20,26 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/hello-friend").withSockJS();
+//        registry.addEndpoint("/hello-friend").withSockJS();
+        registry
+                .addEndpoint("/hello-friend")
+                .setHandshakeHandler(new DefaultHandshakeHandler() {
+                    public boolean beforeHandshake(
+                            ServerHttpRequest request,
+                            ServerHttpResponse response,
+                            WebSocketHandler wsHandler,
+                            Map attributes) throws Exception {
+                        if (request instanceof ServletServerHttpRequest) {
+                            ServletServerHttpRequest servletRequest
+                                    = (ServletServerHttpRequest) request;
+                            HttpSession session = servletRequest
+                                    .getServletRequest().getSession();
+                            attributes.put("sessionId", session.getId());
+                        }
+                        return true;
+                    }
+                }
+                ).withSockJS();
     }
 
     @Override
