aefc0e4bf0bd1b98b214dc41acc3be0bb742c285
Derek Bang <dkbang@umail.ucsb.edu>
2019-04-29 14:30:29 -0700

    join request test

---
+++ src/test/java/edu/ucsb/cs48s19/templates/JoinTest.java
@@ -0,0 +1,37 @@
+package edu.ucsb.cs48s19.templates;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class JoinTest {
+
+    private Message testJoin;
+    private Message secondTestJoin;
+
+     @Before public void setUp() {
+	 testJoin = new JoinRequest("Derek Bang", "test room", "Klingonee", 47);
+     }
+    @Before public void create() {
+	 secondTestJoin = new Message();
+	 secondTestJoin.setUserName("Derek Bang");
+    }
+    
+    @Test
+    public void test_getUserName() {
+	assertEquals("Derek Bang", secondTestJoin.getUserName());
+    }
+
+    @Test
+    public void test_getUserName() {
+	assertEquals(testJoin.getUserName(), secondTestJoin.getUserName());
+    }
+
+    @Test
+    public void test_toString() {
+	assertEquals("JoinRequest{userName=Derek Bang\', roomName='test room\', userLanguage='Klingonee\', request=47}", testJoin.toString());
+    }
+}
---
+++ src/test/java/edu/ucsb/cs48s19/templates/MessageTest.java~
@@ -0,0 +1,23 @@
+package edu.ucsb.cs48s19.templates;
+
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class MessageTest {
+
+    private Message testMessage;
+
+     @Before public void setUp() {
+	 testMessage = new Message("this is a test");
+     }    
+    
+    @Test
+    public void test_getContent() {
+	assertEquals("this is a test", testMessage.getContent());
+    }
+
+}
+    
