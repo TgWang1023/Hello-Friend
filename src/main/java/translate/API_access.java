985ed79a92ffeb799c703a2692ebee17de8fafc8
Tiange Wang <wtgkevin@gmail.com>
2019-05-01 15:04:40 -0700

    Complete API access interface

--- src/main/java/translate/API_access.java
+++ src/main/java/translate/API_access.java
@@ -25,7 +25,7 @@ import java.nio.file.Paths;
 
 public class API_access {
     // text: Original text. source_code: Source language rep code. target_code: Target language rep code
-    // For detailed availibility of rep codes, visit https://cloud.google.com/translate/docs/languages
+    // For detailed availibility of text translation codes, visit https://cloud.google.com/translate/docs/languages
     public static String translate(String text, String source_code, String target_code) throws Exception {
         // Instantiates a client
         Translate translate = TranslateOptions.getDefaultInstance().getService();
@@ -40,8 +40,9 @@ public class API_access {
         return translation.getTranslatedText();
     }
 
-    // file_path: file path to a PCM audio file to transcribe
-    public static ArrayList<String> speech(String file_path) throws Exception {
+    // file_path: File path to a PCM audio file to transcribe. speech_code: Speech language rep code.
+    // For detailed availibility of speech codes, visit https://cloud.google.com/speech-to-text/docs/languages
+    public static ArrayList<String> speech(String file_path, String speech_code) throws Exception {
         try (SpeechClient speech = SpeechClient.create()) {
             Path path = Paths.get(file_path);
             byte[] data = Files.readAllBytes(path);
@@ -51,7 +52,7 @@ public class API_access {
             RecognitionConfig config =
                 RecognitionConfig.newBuilder()
                     .setEncoding(AudioEncoding.LINEAR16)
-                    .setLanguageCode("en-US")
+                    .setLanguageCode(speech_code)
                     .setSampleRateHertz(16000)
                     .build();
             RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
@@ -69,6 +70,50 @@ public class API_access {
             }
 
             return text;
+        }
+    }
+
+    // file_path: file path to a PCM audio file to transcribe. speech_code: Speech language rep code. source_code: Source language rep code. target_code: Target language rep code.
+    // For detailed availibility of text translation codes, visit https://cloud.google.com/translate/docs/languages
+    // For detailed availibility of speech codes, visit https://cloud.google.com/speech-to-text/docs/languages
+    public static ArrayList<String> speech_translate(String file_path, String speech_code, String source_code, String target_code) throws Exception {
+        try (SpeechClient speech = SpeechClient.create()) {
+            Path path = Paths.get(file_path);
+            byte[] data = Files.readAllBytes(path);
+            ByteString audioBytes = ByteString.copyFrom(data);
+        
+            // Configure request with local raw PCM audio
+            RecognitionConfig config =
+                RecognitionConfig.newBuilder()
+                    .setEncoding(AudioEncoding.LINEAR16)
+                    .setLanguageCode("en-US")
+                    .setSampleRateHertz(16000)
+                    .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
+        
+            // Use blocking call to get audio transcript
+            RecognizeResponse response = speech.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+            ArrayList<String> result_text = new ArrayList<String>();
+
+            // Instantiates a client
+            Translate translate = TranslateOptions.getDefaultInstance().getService();
+
+            // Translates some text 
+            for (SpeechRecognitionResult result : results) {
+                // There can be several alternative transcripts for a given chunk of speech. Just use the
+                // first (most likely) one here.
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                String orig_text = alternative.getTranscript().toString();
+                Translation translation =
+                translate.translate(
+                    orig_text,
+                    TranslateOption.sourceLanguage(source_code),
+                    TranslateOption.targetLanguage(target_code));
+                result_text.add(translation.getTranslatedText());
+            }
+
+            return result_text;
         }
     }
 }
