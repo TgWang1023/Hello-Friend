25331a0f0a079b04f8cdad8e398514a0f2d6af03
yujianyu <yujianyu5666@gmail.com>
2019-05-09 20:23:15 -0700

    DD

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,89 +1,43 @@
 var stompClient = null;
-var sessionId = "";
-var url = null
 
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
     if (connected) {
-        $("#room_form").show();
-
+        $("#conversation").show();
     }
     else {
-        $("#section_title").hide();
         $("#conversation").hide();
-        $("#room_form").hide();
-        $("#message_form").hide();
     }
-    $("#conversation").html("");
+    $("#greetings").html("");
 }
 
 function connect() {
-    var socket = new SockJS('/secured/room');
+    var socket = new SockJS('/gs-guide-websocket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
-        url = stompClient.ws._transport.url;
-        url = url.replace(
-            "ws://localhost:8080/secured/room", "");
-        url = url.replace("/websocket", "");
-        url = url.replace(/^[0-9]+\//, "");
-        console.log("Your current session is: " + url);
-        sessionId = url;
         setConnected(true);
-        stompClient.subscribe('/secured/user/queue/specific-room'
-            + '-user' + sessionId, function (greeting) {
-                showMessage(JSON.parse(greeting.body).content);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
         });
     });
 }
 
 function disconnect() {
     if (stompClient !== null) {
-        stompClient.send("/app/secured/user/disconnect" + url, {}, {});
         stompClient.disconnect();
     }
     setConnected(false);
     console.log("Disconnected");
 }
 
-function createRoom() {
-    stompClient.send("/app/secured/user/connect" + url, {}, JSON.stringify(
-        {'userName': $("#user_name").val(),
-        'roomName': $("#room_name").val(),
-        'userLanguage': $("#user_lang").val(),
-        'request': 1}
-    ));
-    $("logo").hide();
-    $("#room_form").hide();
-    $("#message_form").show();
-    $("#section_title").show();
-    $("#conversation").show();
-
+function sendName() {
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
 }
 
-function joinRoom() {
-    stompClient.send("/app/secured/user/connect" + url, {}, JSON.stringify(
-        {'userName': $("#join_user_name").val(),
-        'roomName': $("#join_room_name").val(),
-        'userLanguage': $("#join_user_lang").val(),
-        'request': 2}
-    ));
-    $("logo").hide();
-    $("#room_form").hide();
-
-    $("#message_form").show();
-    $("#section_title").show();
-    $("#conversation").show();
-}
-
-function sendMessage() {
-    stompClient.send("/app/secured/user/send" + url, {}, JSON.stringify(
-        {'content': $("#room_msg").val()}
-    ));
-}
-
-function showMessage(message) {
-    $("#conversation").append("<div class=\"msg_container\">" + message + "</div>");
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
 }
 
 $(function () {
@@ -92,8 +46,5 @@ $(function () {
     });
     $( "#connect" ).click(function() { connect(); });
     $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#create_room" ).click(function() { createRoom(); })
-    $( "#join_room" ).click(function() { joinRoom(); })
-    $( "#send_msg" ).click(function() { sendMessage(); })
-    setConnected(false)
+    $( "#send" ).click(function() { sendName(); });
 });
